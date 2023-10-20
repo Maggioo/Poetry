eb766365ad5ac8d294573199a6bb8a999400ff50
Gabriele <gabriele.maggioni.cologno@gmail.com>
2023-10-20 09:34:36 +0200

    Terzo

---
+++ tests/test_morse.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 20 09:32:32 2023
+
+@author: maggi
+"""
+
+MORSE = {
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    ".": "e",
+    "..-.": "f",
+    "--.": "g",
+    "....": "h",
+    "..": "i",
+    ".---": "j",
+    "-.-": "k",
+    ".-..": "l",
+    "--": "m",
+    "-.": "n",
+    "---": "o",
+    ".--.": "p",
+    "--.-": "q",
+    ".-.": "r",
+    "...": "s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-": "x",
+    "-.--": "y",
+    "--..": "z",
+    "-----": "0",
+    ".----": "1",
+    "..---": "2",
+    "...--": "3",
+    "....-": "4",
+    ".....": "5",
+    "-....": "6",
+    "--...": "7",
+    "---..": "8",
+    "----.": "9",
+}
+
+
+def morse_decoder(code: str) -> str:
+    frase = ''
+    parole = code.split('   ')
+    for el in parole:
+        lettere = el.split()
+        for let in lettere:
+            lettera = MORSE[let]
+            frase = frase + lettera
+        
+        frase = frase + ' '
+    frase = frase[:-1]
+    return frase.capitalize()
+
+def test_answer():
+    assert morse_decoder("... --- -- .   - . -..- -") == "Some text"
+    assert (
+        morse_decoder("..   .-- .- ...   -... --- .-. -.   .. -.   .---- ----. ----. -----")
+        == "I was born in 1990"
+    )
